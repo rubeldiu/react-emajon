37bd19767cfe3d1d9d52510f6af15a5cc2dddb1e
hamiduldiu <hamiduldiu@gmail.com>
2021-03-04 21:02:52 +0100

    some changes

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,7 +6,7 @@ const Header = () => {
     return (
         <div className='header'>
             <img src={logo} alt=""/>
-            <nav><a href="/shop">Shop</a><a href="/review">Review</a><a href="/inventory">Manage Inventory</a></nav>
+            <nav><a href="/shop">Shop</a><a href="/review">Order Review</a><a href="/inventory">Manage Inventory</a></nav>
         </div>
     );
 };
